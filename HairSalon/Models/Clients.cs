293021cf12d36e7a8c19b750a9e4d1584c1facbb
Kennedy777 <reach.marguerite@gmail.com>
2019-05-10 16:15:26 -0700

    add code to Model files

--- HairSalon/Models/Clients.cs
+++ HairSalon/Models/Clients.cs
@@ -5,6 +5,34 @@ namespace HairSalon.Models
 {
 	public class Client
 	{
+     private int _id;
+     private int _stylistId;
+     private string _name;
+     private string _phone;
+     private string _email;
     }
+
+    public void SetId(int id) {_id = id;}
+    public int GetId() {return _id;}
+
+    public void SetStylistId(int stylistId) {_stylistId = stylistId;}
+    public int GetStylistId() {return _stylistID;}
+
+    public void SetName(string name) {_name = name;}
+    public string GetName() {return name}
+
+    public Client(string name, string phone, string email)
+    {
+      SetName(name);
+      SetPhone(phone);
+      SetEmail(email);
+    }
+
+
+	public static void ClearAll()
+	{
+	  Query clearClients = new Clients("DELETE FROM clients");
+      clearClients.Execute();
+	}
 
 }
