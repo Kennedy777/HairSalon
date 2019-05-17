1b30186cba1f170161c3b4de9226e978403aa59f
Kennedy777 <reach.marguerite@gmail.com>
2019-05-17 16:44:23 -0700

    add CSS code

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -25,3 +25,24 @@ namespace HairSalon.Tests
             List<Stylist> allStylists = Stylist.GetAll();
             Assert.AreEqual(1, allStylists.Count);
         }
+
+        [TestMethod]
+        public void Find_StylistInDB_true()
+        {
+            Stylist testStylist = new Stylist("Frederic");
+            testStylist.Save();
+            Stylist findStylist = Stylist.Find(testStylist.GetId());
+            Assert.AreEqual(testStylist.GetName(), findStylist.GetName());
+        }
+
+        [TestMethod]
+        public void ClearAll_ClearStylistsInDB_0()
+        {
+            Stylist testStylist = new Stylist("Harry Cutter");
+            testStylist.Save();
+            Stylist.ClearAll();
+            List<Stylist> allStylists = Stylist.GetAll();
+            Assert.AreEqual(0, allStylists.Count);
+        }
+    } 
+}
--- HairSalon/Models/Clients.cs
+++ HairSalon/Models/Clients.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 
 namespace HairSalon.Models
-{
+
 	public class Clients
 	{
     private int _id;
