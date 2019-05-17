6acd5a5de66dfcbf0a81c4433d35981d73908167
Kennedy777 <reach.marguerite@gmail.com>
2019-05-17 15:20:25 -0700

    add test methods

---
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class ClientTests
+    {
+
+        public ClientTests()
+        {
+            DB.DatabaseTest();
+            Client.ClearAll();
+        }
+
+        [TestMethod]
+        public void SaveAndGetAll_AddClientTooDatabase_1()
+        {
+            Client testClient = new Client("Man", "Phone", "Place", "Notes");
+            testClient.Save(0);
+
+            List<Client> allClients = Client.GetAll();
+            Assert.AreEqual(1, allClients.Count);
+        }
