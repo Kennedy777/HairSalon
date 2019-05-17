b100eb52793248f3833551a069ad7c0a8dc3e2df
Kennedy777 <reach.marguerite@gmail.com>
2019-05-17 16:01:23 -0700

    add more test methods

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -16,11 +16,36 @@ namespace HairSalon.Tests
         }
 
         [TestMethod]
-        public void SaveAndGetAll_AddClientTooDatabase_1()
+        public void GetAllAndSave_NewClientInDatabase_1()
         {
-            Client testClient = new Client("Man", "Phone", "Place", "Notes");
+            Client testClient = new Client("Leela", "2126466800", "New New York");
             testClient.Save(0);
 
             List<Client> allClients = Client.GetAll();
             Assert.AreEqual(1, allClients.Count);
         }
+
+        [TestMethod]
+        public void ClearAll_StylistAfterClear_0()
+        {
+            Stylist testStylist = new Stylist("Hubert");
+            testStylist.Save();
+            Stylist.ClearAll();
+            List<Stylist> allStylists = Stylist.GetAll();
+            Assert.AreEqual(0, allStylists.Count);
+        }
+
+        [TestMethod]
+        public void GetAllAndSave_AtClientToDB_2()
+        {
+            Client testClient = new Client("Frye" "pjf@planetexpress.earth", "9176763486");
+            testClient.Save(0);
+
+            Client testClient2 = new Client("Hermes", "hermes@planetexpress.earth", "9173838448");
+            testClient2.Save(1);
+
+            List<Client> allClients= Client.GetAll();
+            Assert.AreEqual(2, allClients.Count);
+        }
+    }
+}
