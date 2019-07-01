606a3bc31b103c9fd87a59fa2a5e0af604c22cbd
Kennedy777 <reach.marguerite@gmail.com>
2019-07-01 05:50:22 -0700

    revise README

--- marguerite_kennedy.sql
+++ marguerite_kennedy.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 13, 2019 at 01:02 PM
--- Server version: 5.7.24-log
+-- Generation Time: Jun 05, 2019 at 12:45 PM
+-- Server version: 5.7.24
 -- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -25,15 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hairsalon`
+-- Table structure for table `clients`
 --
 
-CREATE TABLE `hairsalon` (
-  `id` int(32) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `phone` varchar(32) NOT NULL,
-  `email` varchar(32) NOT NULL
+CREATE TABLE `clients` (
+  `client_id` bigint(20) UNSIGNED NOT NULL,
+  `client_name` varchar(255) NOT NULL,
+  `client_phone` varchar(10) NOT NULL,
+  `client_email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stylists`
+--
+
+CREATE TABLE `stylists` (
+  `stylist_id` bigint(20) UNSIGNED NOT NULL,
+  `stylist_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD UNIQUE KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `stylists`
+--
+ALTER TABLE `stylists`
+  ADD UNIQUE KEY `stylist_id` (`stylist_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `client_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stylists`
+--
+ALTER TABLE `stylists`
+  MODIFY `stylist_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
